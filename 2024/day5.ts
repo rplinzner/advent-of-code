84c486a5854fbdb9eb4f9ff2762abd50fc0862fb
Rafal Plinzner <rplinzner@gmail.com>
2024-12-05 22:54:27 +0100

    day 5b

--- 2024/day5.ts
+++ 2024/day5.ts
@@ -1,3 +1,5 @@
+import { arrayMoveMutable } from "npm:array-move";
+
 if (import.meta.main) {
   const input = await Deno.readTextFile("./day5.txt");
   const [rulesRaw, messagesRaw] = input.split(/\n\s*\n/);
@@ -7,28 +9,61 @@ if (import.meta.main) {
     .split("\n")
     .map((message) => message.split(",").map(Number));
 
-  const part1 = () => {
-    const isMessageValid = (message: number[]): boolean => {
-      for (const [first, second] of rules) {
-        if (message.indexOf(first) === -1 || message.indexOf(second) === -1) {
-          continue; // Rule cannot be applied
-        }
-        if (message.indexOf(first) >= message.indexOf(second)) {
-          return false;
-        }
+  const isRuleApplicable = (
+    message: number[],
+    [first, second]: number[]
+  ): boolean => message.indexOf(first) !== -1 && message.indexOf(second) !== -1;
+
+  const isMessageValid = (message: number[]): boolean => {
+    for (const [first, second] of rules) {
+      if (!isRuleApplicable(message, [first, second])) {
+        continue; // Rule cannot be applied
       }
+      if (message.indexOf(first) >= message.indexOf(second)) {
+        return false;
+      }
+    }
 
-      return true;
-    };
+    return true;
+  };
 
-    const filtered = messages.filter(isMessageValid);
-
-    const sumOfMiddles = filtered
+  const getSumOfMiddles = (messages: number[][]): number => {
+    return messages
       .map((message) => message[(message.length - 1) / 2])
       .reduce((acc, curr) => acc + curr, 0);
+  };
 
-    console.log(sumOfMiddles);
+  const part1 = () => {
+    const filtered = messages.filter(isMessageValid);
+
+    console.log(getSumOfMiddles(filtered));
+  };
+
+  const part2 = () => {
+    const wrongMessages = messages.filter(
+      (message) => !isMessageValid(message)
+    );
+
+    for (const message of wrongMessages) {
+      do {
+        for (const rule of rules) {
+          if (!isRuleApplicable(message, rule)) continue;
+
+          const [first, second] = rule;
+
+          const firstIndex = message.indexOf(first);
+          const secondIndex = message.indexOf(second);
+
+          if (firstIndex > secondIndex) {
+            arrayMoveMutable(message, secondIndex, firstIndex);
+          }
+        }
+      } while (!isMessageValid(message));
+    }
+
+    console.log(getSumOfMiddles(wrongMessages));
   };
 
   part1();
+  part2();
 }
